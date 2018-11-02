0c026817247b8b9518ea9832e864a3a1607aa741
Ebin Davis <30700182+ebindavis17@users.noreply.github.com>
2018-11-03 00:40:01 +0100

    Update multiAZ_rds-instance_stop.py

--- multiAZ_rds-instance_stop.py
+++ multiAZ_rds-instance_stop.py
@@ -1,6 +1,6 @@
-#This ensure that RDS databases are stopped on a daily basis (can be done by running this script - eg: using CW events)
+#This  ptyhon boto3 script ensures that RDS databases are stopped on a daily basis (can be done by running this script - eg: using CW events)
 #on your AWS accounts ;
-#This helps in unwanted cost utilized on the unused DB instances running on your account
+#This helps in unwanted cost utilized on the unused DB instances running on your account.
 
 #https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds.html
 
