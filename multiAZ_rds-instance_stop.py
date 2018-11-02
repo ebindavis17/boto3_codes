9cbbed8c2282bcc6465aefc1c320eeb6f91b4b50
Ebin Davis <30700182+ebindavis17@users.noreply.github.com>
2018-11-03 00:39:06 +0100

    Update multiAZ_rds-instance_stop.py

--- multiAZ_rds-instance_stop.py
+++ multiAZ_rds-instance_stop.py
@@ -1,30 +1,34 @@
+#This ensure that RDS databases are stopped on a daily basis (can be done by running this script - eg: using CW events)
+#on your AWS accounts ;
+#This helps in unwanted cost utilized on the unused DB instances running on your account
+
+#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds.html
+
+#Note : this script was written when there was no permission given for Multi-AZ DB instance to Stop the instances.
+
 import boto3
-
-dbmultiAZ=[]
-
 
 client = boto3.client('rds')
 
-def lambda_handler(event,context):
-  
-  response = client.describe_db_instances()
-  for i in response['DBInstances']:
-      
-#      if i['MultiAZ'] == True:
-          #print(i['DBInstanceIdentifier'])
-          dbmultiAZ.append(i['DBInstanceIdentifier'])
-          
-'''  
-  for rdsinstances in dbmultiAZ:
-      response1 = client.modify_db_instance(
-          DBInstanceIdentifier=rdsinstances,
-          ApplyImmediately=True,
-          MultiAZ=False
-          )
-'''          
+dbmultiAZ = []
+finalbd = []
+new = []
 
-   response1 = client.stop_db_instance(
-     DBInstanceIdentifier= dbmultiAZ
-     )
-  
- 
+def lambda_handler(event,context):
+    response1=client.describe_db_instances()
+    
+    for i in response1['DBInstances']:
+        if i['DBInstanceStatus'] == 'available':
+            
+            if i['MultiAZ'] == True:
+                response2 = client.modify_db_instance(
+                    DBInstanceIdentifier=i['DBInstanceIdentifier'],
+                    ApplyImmediately=True,
+                    MultiAZ=False
+                    )
+            
+        
+            else:
+                if i['Engine'] != 'aurora':
+                    response3 = client.stop_db_instance(
+                        DBInstanceIdentifier=i['DBInstanceIdentifier'])
